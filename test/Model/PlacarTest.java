7258bd48fdd45a892c830b3a8c3f21036d73a38e
Ramon Vinicius Marqueviski <68443821+RamonMarqueviski@users.noreply.github.com>
2022-05-21 23:19:10 -0300

    Inicial

--- test/Model/PlacarTest.java
+++ test/Model/PlacarTest.java
@@ -46,7 +46,6 @@ public class PlacarTest {
     @Test
      public void verificaTime1VencedorSet() {
         System.out.println("Verifica se há times vencedores - Placar: 26 - 25 - Set 1");
-
         int vencedor = placar.VerificaVencedorSet(26, 25,1);
         assertEquals(0,vencedor);
     }
@@ -56,11 +55,60 @@ public class PlacarTest {
         int vencedor = placar.VerificaVencedorSet(25, 27, 2);
         assertEquals(2,vencedor);
     }
+    @Test
+     public void verificaTimeVencedorSet5() {
+        System.out.println("Verifica se time vencedor é o time2- Placar: 14 - 15 - Set 5");
+        int vencedor = placar.VerificaVencedorSet(14, 15, 5);
+        assertEquals(0,vencedor);
+    }
+    @Test
+     public void verificaTime2VencedorSet5() {
+        System.out.println("Verifica se time vencedor é o time2- Placar: 14 - 16 - Set 5");
+        int vencedor = placar.VerificaVencedorSet(14, 16, 5);
+        assertEquals(2,vencedor);
+    }
        @Test
-     public void verificaTime2VencedorJogo() {
+     public void verificaTime1VencedorJogo() {
         System.out.println("Verifica se time vencedor é o time1 no Jogo");
         int vencedor = placar.VerificaVencedor(3, 0);
         assertEquals(1,vencedor);
     }
+     @Test
+    public void verificaTime2VencedorJogo() {
+        System.out.println("Verifica se time vencedor é o time2 no Jogo");
+        int vencedor = placar.VerificaVencedor(0, 3);
+        assertEquals(2,vencedor);
+    }
+    @Test
+    public void verificaTimeVencedorJogo() {
+        System.out.println("Verifica há times vencedores no Jogo");
+        int vencedor = placar.VerificaVencedor(0, 0);
+        assertEquals(0,vencedor);
+    }
+    @Test
+      public void verificaAdicionaPontosTime1() {
+        System.out.println("Verifica está adicionando pontos");
+        placar.addEquipe1();
+        assertEquals(1,placar.getPontosEquipe1());
+    }
+    @Test
+    public void verificaAdicionaPontosTime2() {
+        System.out.println("Verifica está adicionando pontos");
+        placar.addEquipe2();
+        assertEquals(1,placar.getPontosEquipe2());
+    }
+    @Test
+    public void verificaAdicionaSet2() {
+        System.out.println("Verifica está adicionando Sets");
+        placar.addEquipe2();
+        assertEquals(1,placar.getPontosEquipe2());
+    }
+    @Test
+    public void verificaAdicionaSet1() {
+        System.out.println("Verifica está adicionando Sets");
+        placar.addEquipe1();
+        assertEquals(1,placar.getPontosEquipe1());
+    }
+    
     
 }
